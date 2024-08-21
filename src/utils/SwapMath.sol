14e861f3aab87ad5dc7e07b5ccbc6db220fdff48
Ned Albo <ned.albo@mailfence.com>
2024-08-21 20:30:46 +0200

    changed SwapMath from library to contract

--- src/utils/SwapMath.sol
+++ src/utils/SwapMath.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.8.18;
 
 import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 
-library SwapMath {
+contract SwapMath {
     function sellSlippage(uint256 expected, uint256 actual, uint256 maxBps) public pure returns (uint256) {
         if (actual >= expected) return 0;
         return (expected - actual) * maxBps / expected;
