36958f71ffce0a007fd56865c63a20066daddf88
Ned Albo <ned.albo@mailfence.com>
2024-08-25 20:15:42 +0200

    implemented preview functions for uniswap v3

---
+++ src/interfaces/IUniV3Swapper.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.18;
+
+interface IUniV3Swapper {
+    event SwapFromAToB(address indexed sender, address indexed receiver, uint256 amountIn, uint256 amountOut);
+
+    event SwapFromBToA(address indexed sender, address indexed receiver, uint256 amountIn, uint256 amountOut);
+
+    function tokenA() external view returns (address);
+    function tokenB() external view returns (address);
+
+    function previewSellA(uint256 amountA) external returns (uint256);
+
+    function previewSellB(uint256 amountB) external returns (uint256);
+
+    function previewBuyA(uint256 amountA) external returns (uint256);
+
+    function previewBuyB(uint256 amountB) external returns (uint256);
+
+    function sellA(uint256 amountA, uint256 minAmountB, address receiver) external returns (uint256);
+
+    function sellB(uint256 amountB, uint256 minAmountA, address receiver) external returns (uint256);
+
+    function buyA(uint256 amountA, uint256 maxAmountB, address receiver) external returns (uint256);
+
+    function buyB(uint256 amountB, uint256 maxAmountA, address receiver) external returns (uint256);
+}
--- test/landx/xWheatSwapper.t.sol
+++ test/landx/xWheatSwapper.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.18;
 
+import { console } from "forge-std/src/console.sol";
 import { Test } from "forge-std/src/Test.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { xWheatSwapper } from "@src/landx/xWheatSwapper.sol";
@@ -24,7 +25,22 @@ contract xWheatSwapperTest is Test {
     }
 
     function test_previewSellA() public {
-        uint256 amountOut = swap.previewBuyA(5000 ether);
-        log_uint(amountOut);
+        uint256 amountOut = swap.previewSellA(5000 * (10 ** 6));
+        console.log("Sold 5000 USDC for xWHEAT: ", amountOut);
+    }
+
+    function test_previewSellB() public {
+        uint256 amountOut = swap.previewSellB(1000 * (10 ** 6));
+        console.log("Sold 1000 xWHEAT for USDC: ", amountOut);
+    }
+
+    function test_previewBuyA() public {
+        uint256 amountOut = swap.previewBuyA(5000 * (10 ** 6));
+        console.log("Bought 5000 USDC for xWHEAT: ", amountOut);
+    }
+
+    function test_previewBuyB() public {
+        uint256 amountOut = swap.previewBuyB(1000 * (10 ** 6));
+        console.log("Sold 1000 xWHEAT for USDC: ", amountOut);
     }
 }
